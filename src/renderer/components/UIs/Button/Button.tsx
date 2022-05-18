2e52d6a8f9ae33724bee48b74f57ed9d1e05f704
LIMPIX31 <limpix31@gmail.com>
2022-05-18 10:45:01 +0300

    Reformat Button.tsx

--- src/renderer/components/UIs/Button/Button.tsx
+++ src/renderer/components/UIs/Button/Button.tsx
@@ -6,17 +6,37 @@ import cn from 'classnames'
 export interface ButtonProps {
   icon?: ReactNode | string
   label?: string
-  primary?: boolean,
+  primary?: boolean
   danger?: boolean
   small?: boolean
   disabled?: boolean
 }
 
-export const Button: FC<ClassNamable & OnClickable & ButtonProps> =
-  ({ className, onClick, icon, label, primary, small, disabled }) =>
-    <div className={cn(s.button, small && s.small, primary && s.primary, label && s.hasLabel, disabled && s.disabled, className)} onClick={!disabled ? onClick : () => {}}>
-      {icon && <div className={s.iconWrapper}>
-        {typeof icon === 'string' ? <img src={icon} alt='' /> : icon}
-      </div>}
-      {label && <div className={s.label}>{label}</div>}
-    </div>
+export const Button: FC<ClassNamable & OnClickable & ButtonProps> = ({
+  className,
+  onClick,
+  icon,
+  label,
+  primary,
+  small,
+  disabled,
+}) => (
+  <div
+    className={cn(
+      s.button,
+      small && s.small,
+      primary && s.primary,
+      label && s.hasLabel,
+      disabled && s.disabled,
+      className
+    )}
+    onClick={!disabled ? onClick : () => {}}
+  >
+    {icon && (
+      <div className={s.iconWrapper}>
+        {typeof icon === 'string' ? <img src={icon} alt="" /> : icon}
+      </div>
+    )}
+    {label && <div className={s.label}>{label}</div>}
+  </div>
+)
