e7bfb9d81cccf2595bc2fdce847a8a68a63324ce
LIMPIX31 <limpix31@gmail.com>
2022-05-18 12:14:46 +0300

    base login form

--- src/renderer/components/pages/Home/ProfilePanel/ProfilePanel.tsx
+++ src/renderer/components/pages/Home/ProfilePanel/ProfilePanel.tsx
@@ -10,20 +10,59 @@ import { Button } from '../../../UIs/Button/Button'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faKey } from '@fortawesome/free-solid-svg-icons'
 import { BrandIcon } from '../../../UIs/BrandIcon/BrandIcon'
+import { Field, Formik } from 'formik'
+import * as Yup from 'yup'
 
-const Login: FC = () => <div className={ls.login}>
-  <Title>Login</Title>
-  <Input placeholder={'Login'}/>
-  <Input placeholder={'Password'}/>
-  <div className={ls.actions}>
-    <Button icon={<BrandIcon brand={'discord'}/>} disabled/>
-    <Button icon={<BrandIcon brand={'github'}/>} disabled/>
-    <Button className={ls.mainAction} label={'Login'} primary small icon={<FontAwesomeIcon icon={faKey}/>}/>
+const LoginSchema = Yup.object().shape({
+  login: Yup.string()
+    .required('Required'),
+  password: Yup.string()
+    .required('Required')
+})
+
+const Login: FC = () => (
+  <div className={ls.login}>
+    <Formik
+      validationSchema={LoginSchema}
+      initialValues={{
+        login: '',
+        password: '',
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        console.log(values)
+      }}
+    >
+      {({ submitForm }) => (
+        <>
+          <Title>Login</Title>
+          <Field name={'login'}>
+            {({ field, meta }) => <Input placeholder={'Login'} {...field} error={meta.touched && meta.error} message={meta.error}/>}
+          </Field>
+          <Field name={'password'}>
+            {({ field, meta }) => <Input placeholder={'Password'} {...field} error={meta.touched && meta.error} message={meta.error}/>}
+          </Field>
+          <div className={ls.actions}>
+            <Button icon={<BrandIcon brand={'discord'} />} disabled />
+            <Button icon={<BrandIcon brand={'github'} />} disabled />
+            <Button
+              className={ls.mainAction}
+              label={'Login'}
+              primary
+              small
+              icon={<FontAwesomeIcon icon={faKey} />}
+              onClick={submitForm}
+            />
+          </div>
+        </>
+      )}
+    </Formik>
   </div>
-</div>
+)
 
-export const ProfilePanel: FC<ClassNamable> = ({className}) => {
-  return <RoundContainer className={cn(s.profilecard, className)}>
-    <Login/>
-  </RoundContainer>
+export const ProfilePanel: FC<ClassNamable> = ({ className }) => {
+  return (
+    <RoundContainer className={cn(s.profilecard, className)}>
+      <Login />
+    </RoundContainer>
+  )
 }
