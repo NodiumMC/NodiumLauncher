e27cce4538ce0f406418fec0bd9ed51bc1f74b65
LIMPIX31 <limpix31@gmail.com>
2022-05-19 18:43:33 +0200

    wrap wrapper into wrapper

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,17 +1,28 @@
-import { FC } from 'react'
+import { FC, useEffect } from 'react'
 import s from './style/app.module.scss'
 import { Header } from './components/window/Header/Header'
 import { Home } from './components/pages/Home/Home'
 import { ObserverComponent } from './components/utils/ObserverComponent'
 import { AppPreloader } from './components/containers/AppPreloader/AppPreloader'
+import { Route, Routes, useNavigate } from 'react-router-dom'
 
 export const App: FC = ObserverComponent(() => {
+  const nav = useNavigate()
+
+  useEffect(() => {
+    nav('/', { replace: true })
+  },[])
+
   return (
     <div className={s.appWindow}>
       <Header />
       <div className={s.view}>
         <AppPreloader />
-        <Home />
+        <div className={s.page}>
+          <Routes>
+            <Route path={'/'} element={<Home />}/>
+          </Routes>
+        </div>
       </div>
     </div>
   )
