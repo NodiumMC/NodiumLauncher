4911760f7e23c2716c2b7bf12b8a995e9218b579
LIMPIX31 <limpix31@gmail.com>
2022-05-21 16:16:32 +0200

    signal in header

--- src/renderer/components/window/Header/Header.tsx
+++ src/renderer/components/window/Header/Header.tsx
@@ -1,14 +1,22 @@
 import { FC } from 'react'
 import s from './Header.module.scss'
 import { WindowButton } from '../WindowButton/WindowButton'
+import { Signal } from '../../UIs/Signal/Signal'
+import { MathUtils } from '../../../../app/utils/MathUtils'
+import { useApiStatus } from '../../../hooks/useApi'
+import { ObserverComponent } from '../../utils/ObserverComponent'
 
-export const Header: FC = () => {
-  return <div className={s.header}>
-    <div></div>
-    <div style={{display: 'flex'}}>
-      <WindowButton type={'minimize'}/>
-      <WindowButton type={'maximize'}/>
-      <WindowButton type={'close'}/>
+export const Header: FC = ObserverComponent(() => {
+    const status = useApiStatus()
+
+    return <div className={s.header}>
+      <div></div>
+      <div style={{display: 'flex', alignItems: 'center'}}>
+        <Signal tiles={Math.floor(MathUtils.map(status.lastPing, 500, 0, 1, 3))}/>
+        <WindowButton type={'minimize'}/>
+        <WindowButton type={'maximize'}/>
+        <WindowButton type={'close'}/>
+      </div>
     </div>
-  </div>
-}
+  }
+)
