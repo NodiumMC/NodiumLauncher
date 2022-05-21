89de0da76c447121d7dc32faf58d19232e7b6d50
LIMPIX31 <limpix31@gmail.com>
2022-05-21 16:06:44 +0200

    ugrade + fix APIProvider

--- src/app/API/APIProvider.ts
+++ src/app/API/APIProvider.ts
@@ -1,21 +1,20 @@
 import { Service } from 'typedi'
 import {
   ApolloClient,
+  createHttpLink,
   InMemoryCache,
   NormalizedCacheObject,
 } from '@apollo/client'
-import axios, { AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios'
+import axios, { AxiosError, AxiosInstance, AxiosRequestConfig } from 'axios'
 import { LocalStorage } from '../localstorage/LocalStorage.class'
 import { AccessTokenModel } from '../localstorage/models/AccessToken.model'
 import lf from 'lowfun'
+import { setContext } from '@apollo/client/link/context'
 
 @Service()
 export class APIProvider {
   static BASE_URL: string = 'http://localhost:4044/'
-  readonly apollo: ApolloClient<NormalizedCacheObject> = new ApolloClient({
-    uri: APIProvider.BASE_URL + 'graphql',
-    cache: new InMemoryCache(),
-  })
+  readonly apollo: ApolloClient<NormalizedCacheObject>
   readonly axios: AxiosInstance = axios.create({
     baseURL: APIProvider.BASE_URL,
     timeout: 10000,
@@ -23,8 +22,28 @@ export class APIProvider {
   })
 
   constructor(private ls: LocalStorage) {
-    this.axios.interceptors.request.use(this.requestInterceptor)
-    this.axios.interceptors.response.use(lf, this.responseInterceptor)
+    this.apollo = new ApolloClient({
+      link: createHttpLink({
+        uri: APIProvider.BASE_URL + 'graphql',
+        credentials: 'include',
+      }).concat(
+        setContext((_, { headers }) => {
+          const token = ls.getModel(AccessTokenModel).accessToken
+          return {
+            headers: {
+              ...headers,
+              Authorization: token ? `Bearer ${token}` : '',
+            },
+          }
+        })
+      ),
+      cache: new InMemoryCache(),
+    })
+    this.axios.interceptors.request.use(this.requestInterceptor.bind(this))
+    this.axios.interceptors.response.use(
+      lf,
+      this.responseInterceptor.bind(this)
+    )
   }
 
   private requestInterceptor(config: AxiosRequestConfig): AxiosRequestConfig {
@@ -37,26 +56,32 @@ export class APIProvider {
 
   private async responseInterceptor(error: AxiosError) {
     const originalRequest = error.config
-    let isRetry = false
-    if (error.config && error.response?.status === 401 && !isRetry)
-      if(await this.refresh()) await axios.request(originalRequest)
-    else throw error
+    if (
+      error.config &&
+      error.response?.status === 401 &&
+      !error.config['isRetry']
+    ) {
+      originalRequest['isRetry'] = true
+      if (await this.refresh()) return await axios.request(originalRequest)
+    } else throw error
   }
 
   async refresh(): Promise<boolean> {
     try {
       this.ls.getModel(AccessTokenModel).accessToken = await axios
-        .get(`${APIProvider.BASE_URL}api/refresh`, {
+        .get(`${APIProvider.BASE_URL}auth/refresh`, {
           withCredentials: true,
           timeout: 5000,
         })
         .then(res => {
           const token = res.data?.data?.accessToken
-          if(!token) throw new Error('Missing token')
+          if (!token) throw new Error('Missing token')
           return token
         })
       await this.ls.saveModel(AccessTokenModel)
       return true
-    } catch (e) { return false }
+    } catch (e) {
+      return false
+    }
   }
 }
